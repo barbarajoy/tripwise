ed3da26e57e2cee0d9d7621b749fab2eb4176093
Laura Beauvais <lauralisa.beauvais@gmail.com>
2023-08-30 11:27:33 +0200

    trip_id foreign key created through migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_095254) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_092519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_095254) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "trip_id", null: false
+    t.index ["trip_id"], name: "index_destinations_on_trip_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -62,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_095254) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "destinations", "trips"
   add_foreign_key "messages", "trips"
   add_foreign_key "messages", "users"
   add_foreign_key "trips", "trips"
