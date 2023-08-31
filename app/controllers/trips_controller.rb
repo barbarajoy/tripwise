0dacd6b1b778cb8a1d1997fecf8564b40ccd7cdd
Barbara Joy <barbarajoydneves@gmail.com>
2023-08-31 15:30:33 +0200

    done

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,9 @@ class TripsController < ApplicationController
 
   def index
     @trips = Trip.all
+    if params.dig(:filter, :style)
+      @trips = @trips.where(style: params[:filter][:style])
+    end
   end
 
   def show
