97e70a696a23cac0b82fb5eb0d6781c4bf352364
Barbara Joy <barbarajoydneves@gmail.com>
2023-09-25 15:20:07 +0200

    made some changes for responsive design and took off authentication from index and show of trips

--- config/routes.rb
+++ config/routes.rb
@@ -14,16 +14,3 @@ Rails.application.routes.draw do
     resources :trips, only: %i[index show new create edit update destroy]
   end
 end
-
-
-# root to: "cats#index"
-# resources :cats, only: %i[index show new create] do
-#   resources :reservations, only: %i[new]
-#   resources :reservations, only: %i[create], path: ''
-# end
-# namespace :admin do
-#   resources :cats, only: %i[index show]
-# end
-# devise_for :users, controllers: {
-#   sessions: 'users/sessions'
-# }
